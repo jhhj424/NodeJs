5768ceb175d7539045d6dd245516c3210021a924
dev_jito <jhhj424@naver.com>
2020-06-07 08:52:13 +0000

    NodeJs

---
+++ javascript/step14_for.js
@@ -0,0 +1,8 @@
+var array = new Array();
+
+for (var i = 0; i < 10; i++) {
+	array.push(i);
+}
+
+console.log(array.toString());
+// "0,1,2,3,4,5,6,7,8,9"
---
+++ javascript/step16_while.js
@@ -0,0 +1,21 @@
+var i = 0;
+
+while (i < 10) {
+	i++; 
+}
+
+console.log(i);
+// 10
+
+
+
+//----------- Do While
+
+var i = 0; 
+
+do {
+  i++;
+} while (i < 10)
+  
+console.log(i)
+// 10
